97d8afadda2e28099bd75aa56185333ccf303b86
raptazure <raptazure@foxmail.com>
2020-11-29 16:31:59 +0800

    tutorial: select

--- tutorial/select.rs
+++ tutorial/select.rs
@@ -125,4 +125,69 @@ impl Future for MySelect {
 //     Ok(())
 // }
 
+// #[tokio::main]
+// async fn main() {
+//     let (tx1, mut rx1) = mpsc::channel(128);
+//     let (tx2, mut rx2) = mpsc::channel(128);
+//     let (tx3, mut rx3) = mpsc::channel(128);
+
+//     loop {
+//         let msg = tokio::select! {
+//             Some(msg) = rx1.recv() => msg,
+//             Some(msg) = rx2.recv() => msg,
+//             Some(msg) = rx3.recv() => msg,
+//             else => { break }
+//         };
+
+//         println!("Got {:?}", msg);
+//     }
+
+//     println!("All channels have been closed.");
+// }
+
+// async fn action(input: Option<i32>) -> Option<String> {
+//     // If the input is `None`, return `None`.
+//     // This could also be written as `let i = input?;`
+//     let i = match input {
+//         Some(input) => input,
+//         None => return None,
+//     };
+//     // async logic here
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     let (mut tx, mut rx) = tokio::sync::mpsc::channel(128);
+
+//     let mut done = false;
+//     let operation = action(None);
+//     tokio::pin!(operation);
+
+//     tokio::spawn(async move {
+//         let _ = tx.send(1).await;
+//         let _ = tx.send(3).await;
+//         let _ = tx.send(2).await;
+//     });
+
+//     loop {
+//         tokio::select! {
+//             res = &mut operation, if !done => {
+//                 done = true;
+
+//                 if let Some(v) = res {
+//                     println!("GOT = {}", v);
+//                     return;
+//                 }
+//             }
+//             Some(v) = rx.recv() => {
+//                 if v % 2 == 0 {
+//                     // `.set` is a method on `Pin`.
+//                     operation.set(action(Some(v)));
+//                     done = false;
+//                 }
+//             }
+//         }
+//     }
+// }
+
 fn main() {}
